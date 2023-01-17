83cf210147d2dd263fbf319439e3d3a42d3dd8e9
Aksel4 <aklua001@osloskolen.no>
2023-01-17 14:23:34 +0100

    før flere baller prøve

--- sketch.js
+++ sketch.js
@@ -2,42 +2,51 @@ let sirkel = 3
 let xpos = 400
 let ypos = 300
 let størrelse = 0
-let hp = 3
+let hp = 1
 let score = 0
 let ekstrahastighetx = -0.5
 let ekstrahastighety = 0.5
 let økningshastighet = .2
-let antallBalls = 1
+let antallBalls = 2
 let poeng = 0
 let balls = []
+let vanskelighetsgrad = 1
 
 function setup() { 
   cnv = createCanvas(1300, 600); 
   cnv.mousePressed(clicked)
+  cnv
+  noCursor();
+  alert('--> click on the asteroid to destroy it                                                           -->arrow up increases difficulty and arrow down decreases difficulty')
 
 } 
+key
+function keyPressed() {
+  if(keyCode === UP_ARROW && vanskelighetsgrad < 3) {
+    vanskelighetsgrad = vanskelighetsgrad + 1
 
-function preLoad() {
-  img = loadImage('Bilder/Stars.jpg') 
+  } else if (keyCode === DOWN_ARROW && vanskelighetsgrad > 1) {
+    vanskelighetsgrad = vanskelighetsgrad -1
+  }
+
+
 }
 
 function clicked() {
   let avstand = dist(xpos, ypos, mouseX, mouseY)
   let radius = størrelse/2
 
-  if(avstand <= radius) {  
+  if(avstand <= radius) {
     fill(60)
     størrelse = 0
     ekstrahastighetx = random(-0.7, 0.7)
     ekstrahastighety = random(-0.7, 0.7)
     xpos = random(300, 1000);
     ypos = random(200, 400);
-    økningshastighet = økningshastighet + 0.1
+    økningshastighet = økningshastighet + 0.5
     poeng = poeng + 1
   }
 }
-
-
 
 function draw() { 
   noStroke()
@@ -69,21 +78,26 @@ function draw() {
   if(poeng == 5) {
     antallBalls = 2
   }
-  if(hp == 2) {
-    stroke(255,255,0)
-  } 
-  if(hp == 1) { 
-    stroke(255, 0, 0)
+  if(vanskelighetsgrad == 3) {
+    stroke(255,0,0)
+    økningshastighet = 6
+   
   }
-  if (hp == 0) {
-    fill(200)
-    økningshastighet = 1
-    hp = 3
-    størrelse = 25
-    fill(60)
-    poeng = 0
-  }
+  if(vanskelighetsgrad == 2) {
+    stroke(255, 255, 0)
+    økningshastighet = 4
 
+  }
+  if (vanskelighetsgrad == 1) {
+    stroke(0,255,0)
+    økningshastighet = 2
+    }
+
+  if (hp == 0) {
+    størrelse = 25
+    poeng = 0
+    hp = 1
+  }
 
   line(mouseX+22,mouseY+22,mouseX-22,mouseY-22) 
   line(mouseX-22,mouseY+22,mouseX+22,mouseY-22)
