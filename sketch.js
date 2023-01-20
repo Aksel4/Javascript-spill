3641365523dc7ba3867f0b43797feed894ab957b
Aksel4 <aklua001@osloskolen.no>
2023-01-20 09:58:58 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,12 +15,21 @@ let vanskelighetsgrad = 1
 function setup() { 
   cnv = createCanvas(1300, 600); 
   cnv.mousePressed(clicked)
-  cnv
   noCursor();
   alert('--> click on the asteroid to destroy it                                                           -->arrow up increases difficulty and arrow down decreases difficulty')
 
 } 
-key
+
+//respawner ballen
+function respawn() {
+  størrelse = 0
+  ekstrahastighetx = random(-0.7, 0.7)
+  ekstrahastighety = random(-0.7, 0.7)
+  xpos = random(300, 1000);
+  ypos = random(200, 400);
+}
+
+//vanskelighetsgrad funksjon
 function keyPressed() {
   if(keyCode === UP_ARROW && vanskelighetsgrad < 3) {
     vanskelighetsgrad = vanskelighetsgrad + 1
@@ -31,18 +40,13 @@ function keyPressed() {
 
 
 }
-
+//musepress funksjon
 function clicked() {
   let avstand = dist(xpos, ypos, mouseX, mouseY)
   let radius = størrelse/2
 
-  if(avstand <= radius) {
-    fill(60)
-    størrelse = 0
-    ekstrahastighetx = random(-0.7, 0.7)
-    ekstrahastighety = random(-0.7, 0.7)
-    xpos = random(300, 1000);
-    ypos = random(200, 400);
+  if(avstand <= radius) { 
+    respawn()
     økningshastighet = økningshastighet + 0.5
     poeng = poeng + 1
   }
@@ -50,27 +54,26 @@ function clicked() {
 
 function draw() { 
   noStroke()
-  background(30)
+  background(0,0,60)
   xpos = xpos + ekstrahastighetx
   ypos = ypos + ekstrahastighety
   størrelse = størrelse + økningshastighet
 
+  // for å få flere baller, fortsetter på en annen branch
   for(let i = 0; i < antallBalls; i++) {
-    fill(240)
+    fill(180)
     circle(xpos, ypos, størrelse)
     
   }
 
+  //poengsystem
   let s = 'Poengsum ' + poeng;
   fill(255);
   text(s, 10, 10, 100, 100);
 
+  //skjekker om ballen er for stor
   if(størrelse >= 300) {
-    størrelse = 25
-    xpos = random(300, 1000);
-    ypos = random(200, 400);
-    ekstrahastighetx = random(-1, 1)
-    ekstrahastighety = random(-1, 1)
+    respawn()
     hp = hp -1
     økningshastighet = 1
   }
@@ -78,27 +81,31 @@ function draw() {
   if(poeng == 5) {
     antallBalls = 2
   }
+
+  //vanskelighetsgrad regler
   if(vanskelighetsgrad == 3) {
     stroke(255,0,0)
-    økningshastighet = 6
+    økningshastighet = 10
    
   }
   if(vanskelighetsgrad == 2) {
     stroke(255, 255, 0)
-    økningshastighet = 4
+    økningshastighet = 6
 
   }
   if (vanskelighetsgrad == 1) {
     stroke(0,255,0)
-    økningshastighet = 2
+    økningshastighet = 4
     }
 
+    //skjekker om hp er for lav
   if (hp == 0) {
     størrelse = 25
     poeng = 0
     hp = 1
   }
 
+  //crosshair
   line(mouseX+22,mouseY+22,mouseX-22,mouseY-22) 
   line(mouseX-22,mouseY+22,mouseX+22,mouseY-22)
 }
